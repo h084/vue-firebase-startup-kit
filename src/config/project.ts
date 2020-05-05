cb13b7e0eaefb6a5ee09c593251dfea0d546b69c
丸橋裕生 (MARUHASHI, Hiroki) <h084@users.noreply.github.com>
2020-05-05 16:16:57 +0900

    add project.ts

---
+++ src/config/project.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAxhEdTtYYJf5tcfgbisZENeIFyUiM0rCY",
+  authDomain: "view-firebase-startup-kit.firebaseapp.com",
+  databaseURL: "https://view-firebase-startup-kit.firebaseio.com",
+  projectId: "view-firebase-startup-kit",
+  storageBucket: "view-firebase-startup-kit.appspot.com",
+  messagingSenderId: "877288309692",
+  appId: "1:877288309692:web:db0833beb9577877c121b9"
+};
